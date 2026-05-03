fe851a778ae618fb5a8a8cfc59c6a2c924f52368
Nikolai <spamemaildlyaigr@gmail.com>
2026-05-03 13:32:39 +0200

    but what if headless is the way?

--- src/main/kotlin/frontend/helpers/TestBaseUI.kt
+++ src/main/kotlin/frontend/helpers/TestBaseUI.kt
@@ -12,6 +12,7 @@ open class TestBaseUI {
         Configuration.pageLoadStrategy = "normal"
         Configuration.reopenBrowserOnFail = true
         Configuration.timeout = 10000
+        Configuration.headless = true
         //Configuration.browser = DriverProvider::class.java.name
     }
 
